77d8eacf1069380d5ed099c43dd2a2f0bd933629
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-19 20:34:01 +0900

    feat - [Unit2]textfield onValueChange 설정

--- Unit2/Pathway3/Tip-Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/Tip-Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,18 +35,26 @@ fun Caculate(modifier: Modifier) {
     Column(
         modifier = modifier.padding(32.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(25.dp)
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
         Text(text = stringResource(R.string.title), style = TextStyle(
             fontSize = 20.sp
         ))
-        TextField(value = TextFieldValue(stringResource(R.string.hint_text)), onValueChange = {})
+        EditNumberField()
         Text(text = stringResource(R.string.result), style = TextStyle(
             fontSize = 18.sp, fontWeight = FontWeight.Bold
         ))
     }
 }
-
+@Composable
+fun EditNumberField(){
+    var number by remember { mutableStateOf("") }
+    TextField(
+        value = number,
+        onValueChange = {
+            number = it
+        })
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreiview() {
