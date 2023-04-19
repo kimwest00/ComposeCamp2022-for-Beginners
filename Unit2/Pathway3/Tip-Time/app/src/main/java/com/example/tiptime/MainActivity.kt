c65dbb8167b678ea36b50d0fd167d7a2f130dfaf
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-19 21:24:30 +0900

    feat - tip calculate function 추가 + textfield 속성 추가

--- Unit2/Pathway3/Tip-Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/Tip-Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.material.Text
@@ -16,10 +17,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,19 +44,36 @@ fun Caculate(modifier: Modifier) {
             fontSize = 20.sp
         ))
         EditNumberField()
-        Text(text = stringResource(R.string.result), style = TextStyle(
+        Text(text = stringResource(R.string.result,""), style = TextStyle(
             fontSize = 18.sp, fontWeight = FontWeight.Bold
         ))
     }
 }
+ㅔ
 @Composable
 fun EditNumberField(){
     var number by remember { mutableStateOf("") }
+    val numberDoubleType = number.toDoubleOrNull() ?:0.0
+    val tip = calculateTip(numberDoubleType)
     TextField(
+        // Other parameters
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         value = number,
+        //TODO: text @Composable invocatins can only happen from context of a @Composable function 에러 수정
+        label = { Text(text = stringResource(R.string.hint_text)) },
         onValueChange = {
             number = it
-        })
+        },
+}
+private fun calculateTip(
+    amount:Double,
+    tipPercent:Double = 15.0
+):String{
+    val tip = amount / 100*amount
+    //통화 형식으로 지정
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 @Preview(showBackground = true)
 @Composable
