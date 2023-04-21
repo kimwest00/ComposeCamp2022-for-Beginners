1c40994641dae5f61c91e53a13d0424e85d3a1c0
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-22 03:16:34 +0900

    feat - [ Unit3 ]list LazyColumn으로 구현

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -22,16 +22,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmationscodelab.data.Datasource
@@ -54,11 +53,19 @@ fun AffirmationApp() {
     AffirmationsTheme {
         // card UI test
         val dataSource = Datasource().loadAffirmations()
-        Card(dataSource[1])
+        AffirmationList(dataSource)
     }
 }
 @Composable
-fun Card(affirmation: Affirmation,modifier: Modifier = Modifier){
+private fun AffirmationList(affirmationList:List<Affirmation>,modifier: Modifier=Modifier){
+    LazyColumn {
+        items(affirmationList){ affirmation ->
+            AffirmationsCard(affirmation)
+        }
+    }
+}
+@Composable
+fun AffirmationsCard(affirmation: Affirmation,modifier: Modifier = Modifier){
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
