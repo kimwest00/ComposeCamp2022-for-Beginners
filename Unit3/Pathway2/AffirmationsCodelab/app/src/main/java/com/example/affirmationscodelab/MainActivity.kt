6f9647ed6ce1aabf690b40a0df81714d55177929
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-22 03:02:59 +0900

    feat - [Unit3]Card 위젯으로 UI 구성+ data class 추가

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -17,8 +17,25 @@ package com.example.affirmationscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationscodelab.data.Datasource
+import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +47,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Preview
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+        // card UI test
+        val dataSource = Datasource().loadAffirmations()
+        Card(dataSource[1])
     }
+}
+@Composable
+fun Card(affirmation: Affirmation,modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Card(
+            modifier = modifier.padding(8.dp), elevation = 4.dp
+        ){
+            Column {
+                Image(
+                    painter = painterResource(affirmation.imageResourceId)
+                    ,contentDescription = stringResource(affirmation.stringResourceId),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(190.dp),
+                    contentScale = ContentScale.Crop
+
+                )
+                Text(
+                    text =stringResource(affirmation.stringResourceId),
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.h6
+                )
+
+            }
+        }
+    }
+
 }
