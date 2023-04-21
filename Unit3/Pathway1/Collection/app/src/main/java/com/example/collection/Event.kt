044e22c209e410b6a3ca10e83d95260eaefccfd7
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-22 02:21:21 +0900

    feat - [Unit3] enum, eventList로 추가

--- Unit3/Pathway1/Collection/app/src/main/java/com/example/collection/Event.kt
+++ Unit3/Pathway1/Collection/app/src/main/java/com/example/collection/Event.kt
@@ -1,18 +1,27 @@
 package com.example.collection
 
+enum class Daypart{
+    MORNING,
+    AFTERNOON,
+    EVENING,
+}
 data class Event(
     val title: String,
     val description: String? = null,
-    val daypart: String,
+    val daypart: Daypart,
     val durationInMinutes: Int,
 )
+
 fun main(){
-    val event = Event(
-        title = "Study Kotlin",
-        description="Commit to studying Kotlin at least 15 minutes per day.",
-        daypart = "Evening",
-        durationInMinutes=15
+    var eventList = mutableListOf<Event>(
+        Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0),
+        Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15),
+        Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30),
+        Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60),
+        Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10),
+        Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
     )
-    print(event)
+
+    print(eventList)
 
 }
