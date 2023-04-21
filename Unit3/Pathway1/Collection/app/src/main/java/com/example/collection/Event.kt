3c51314080456ab39eee36ddd328d21720e95d5c
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-22 02:46:09 +0900

    feat - [Unit3] 고차함수 활용 예제 추가

--- Unit3/Pathway1/Collection/app/src/main/java/com/example/collection/Event.kt
+++ Unit3/Pathway1/Collection/app/src/main/java/com/example/collection/Event.kt
@@ -11,7 +11,12 @@ data class Event(
     val daypart: Daypart,
     val durationInMinutes: Int,
 )
-
+val Event.durationOfEvent:String
+    get() = if(this.durationInMinutes < 60){
+        "short"
+    }else{
+        "long"
+    }
 fun main(){
     var eventList = mutableListOf<Event>(
         Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0),
@@ -21,7 +26,24 @@ fun main(){
         Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10),
         Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
     )
+    //1. daypart대로 collection 분리
+    val groupEvent = eventList.groupBy {
+        it.daypart
+    }
+    groupEvent.forEach{(daypart,event)->
+        println("${daypart}:${event.size}")
+    }
+    //2. 60분보다 짧은 event list 반환및 시간순으로 정렬
+    var filterEvent = eventList.filter {
+        it.durationInMinutes<60
+    }
+    filterEvent.sortedBy {
+        it.durationInMinutes
+    }
+    //3. 마지막 인덱스 출력
+    print(eventList.last())
+    //4. collection 확장하기
 
-    print(eventList)
+    println("Duration of first event of the day: ${eventList.last().durationOfEvent}")
 
 }
