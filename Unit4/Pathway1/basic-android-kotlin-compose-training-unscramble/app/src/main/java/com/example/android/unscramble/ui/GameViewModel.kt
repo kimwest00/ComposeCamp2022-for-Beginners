6847931b6363927a7449c8f08a113ddd7dfe1f46
kimwest00 <minseohufs@hufs.ac.kr>
2023-04-26 03:03:16 +0900

    feat - [Unit4] viewmodel 연결 + currentScrambleWord 변수 연결

---
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.android.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.example.android.unscramble.data.allWords
+
+// Game UI state
+
+
+class GameViewModel:ViewModel() {
+    data class GameUiState(
+        val currentScrambledWord: String = ""
+    )
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    init {
+        resetGame()
+    }
+    //게임을 초기화하는 함수
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    //1. 게임 대상이 되는 정답
+    private lateinit var currentWord: String
+    //2. Set of words used in the game
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    //게임에서 아직 제시되지않은 단어를
+    //usedWords에 있는지 판별하고 없다면 currentWord로 선언하는 함수
+    private fun pickRandomWordAndShuffle(): String {
+        // Continue picking up a new random word until you get one that hasn't been used before
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+    ///정답이 되는 단어를 퀴즈 대상이 되도록
+    // 순서를 random하게 섞는 함수
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+
+}
